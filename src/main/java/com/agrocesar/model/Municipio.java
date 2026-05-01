1d460f4238f2b78158a78a02eda65d27d701947b
Abisael Freyle Almenarez <afreylea@unicesar.edu.co>
2026-05-01 13:33:20 -0500

    feat(model): se agregaron todas las 3 clases restantes del modelo, y se aplicaron modificaciones a los existentes

--- src/main/java/com/agrocesar/model/Municipio.java
+++ src/main/java/com/agrocesar/model/Municipio.java
@@ -1,5 +1,7 @@
 package com.agrocesar.model;
 
+import java.time.LocalDateTime;
+
 public class Municipio {
 
     private Long id;
@@ -8,4 +10,62 @@ public class Municipio {
     private Double latitud;
     private Double longitud;
     private Integer activo;
+    private LocalDateTime fechaCreacion;
+
+    public Municipio () { }
+
+    public Municipio (Long id, String nombre, String departamento, 
+                      Double latitud, Double longitud, Integer activo, 
+                      LocalDateTime fechaCreacion ) {
+        
+        this.id            = id;
+        this.nombre        = nombre;
+        this.departamento  = departamento;
+        this.latitud       = latitud;
+        this.longitud      = longitud;
+        this.activo        = activo;
+        this.fechaCreacion = fechaCreacion;               
+    }
+
+    public Long getId ()                     { return id; }
+    public String getNombre ()               { return nombre; }
+    public String getDepartamento ()         { return departamento; }
+    public Double getLatitud ()              { return latitud; }
+    public Double getLongitud ()             { return longitud; }
+    public Integer getActivo ()              { return activo; }
+    public LocalDateTime getFechaCreacion () { return fechaCreacion; }
+
+    public void setId (Long id)                                {this.id = id; }
+    public void setNombre (String nombre)                      { this.nombre = nombre; }
+    public void setDepartamento (String departamento)          { this.departamento = departamento; }
+    public void setLatitud (Double latitud)                    { this.latitud = latitud; }
+    public void setLongitud (Double longitud)                  { this.longitud = longitud; }
+    public void setActivo (Integer activo)                     { this.activo = activo; }
+    public void setFechaCreacion (LocalDateTime fechaCreacion) { this.fechaCreacion = fechaCreacion; }
+
+    public static Builder builder () { return new Builder(); }
+
+    public static class Builder {
+
+        private Long id;
+        private String nombre;
+        private String departamento;
+        private Double latitud;
+        private Double longitud;
+        private Integer activo;
+        private LocalDateTime fechaCreacion;
+
+        public Builder id (Long id)                                { this.id = id; return this; }
+        public Builder nombre (String nombre)                      { this.nombre = nombre; return this; }
+        public Builder departamento (String departamento)          { this.departamento = departamento; return this; }
+        public Builder latitud (Double latitud)                    { this.latitud = latitud; return this; }
+        public Builder Longitud (Double longitud)                  { this.longitud = longitud; return this;}
+        public Builder activo (Integer activo)                     { this.activo = activo; return this; }
+        public Builder fechaCreacion (LocalDateTime fechaCreacion) { this.fechaCreacion = fechaCreacion; return this; }
+
+        public Municipio build () {
+            return new Municipio(id, nombre, departamento, latitud,
+                                 longitud, activo, fechaCreacion );
+        }
+    }
 }

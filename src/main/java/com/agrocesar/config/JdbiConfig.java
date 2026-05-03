499090f2e09b168ecc4b129f11e0d9bf869886e1
kstrodeveloper <aynerjosec@gmail.com>
2026-05-03 08:25:14 -0500

    fix: realizar multiples ajustes para poner desplegar el proyecto localmente.

--- src/main/java/com/agrocesar/config/JdbiConfig.java
+++ src/main/java/com/agrocesar/config/JdbiConfig.java
@@ -3,22 +3,33 @@ package com.agrocesar.config;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
+import org.springframework.beans.factory.annotation.Value;
 import com.agrocesar.repository.UsuarioRepository;
 
-import javax.sql.DataSource;
-
 @Configuration
-@Profile("!nobd")
-
 public class JdbiConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
 
     @Bean
     @ConditionalOnMissingBean(Jdbi.class)
-    public Jdbi jdbi(DataSource dataSource) {
+    public Jdbi jdbi() {
+        javax.sql.DataSource dataSource = DataSourceBuilder.create()
+            .url(url)
+            .username(username)
+            .password(password)
+            .driverClassName("oracle.jdbc.OracleDriver")
+            .build();
         Jdbi jdbi = Jdbi.create(dataSource);
         jdbi.installPlugin(new SqlObjectPlugin());
         return jdbi;

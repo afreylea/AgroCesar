1b861b2a316c531bb3623b50925993fdc640e852
kstrodeveloper <aynerjosec@gmail.com>
2026-05-09 15:38:41 -0500

    fix: - Agregar @SpringBootApplication faltante en AgrocesarApplication - Agregar spring.main.web-application-type=servlet en properties - Excluir reactor-netty de webflux para evitar conflicto con Tomcat - Eliminar dependencia de HttpClient de Netty en WebClientConfig"

--- src/main/java/com/agrocesar/AgrocesarApplication.java
+++ src/main/java/com/agrocesar/AgrocesarApplication.java
@@ -1,7 +1,10 @@
 package com.agrocesar;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 public class AgrocesarApplication {
 
     public static void main(String[] args) {

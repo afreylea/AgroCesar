212cc453bafbd14c6d24e3a8d2609ad520afce48
kstrodeveloper <aynerjosec@gmail.com>
2026-05-18 16:27:55 -0500

    feat(config): registrar bean de catalogo en Jdbi config

--- src/main/java/com/agrocesar/config/JdbiConfig.java
+++ src/main/java/com/agrocesar/config/JdbiConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 
+import com.agrocesar.repository.CatalogoRepository;
 import com.agrocesar.repository.CultivoAgricultorRepository;
 import com.agrocesar.repository.MunicipioRepository;
 import com.agrocesar.repository.UsuarioRepository;
@@ -49,5 +50,10 @@ public class JdbiConfig {
     @Bean
     public CultivoAgricultorRepository cultivoAgricultorRepository(Jdbi jdbi){
         return jdbi.onDemand(CultivoAgricultorRepository.class);
+    }
+
+    @Bean
+    public CatalogoRepository catalogoRepository(Jdbi jdbi){
+        return jdbi.onDemand(CatalogoRepository.class);
     }
 }

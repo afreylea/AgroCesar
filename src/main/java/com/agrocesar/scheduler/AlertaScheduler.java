55da945f63a2afa7acac85c1905521f05277d19c
kstrodeveloper <aynerjosec@gmail.com>
2026-05-26 19:23:00 -0500

    feat(scheduler): agregar logs a los métodos de ejecución programada en AlertaScheduler

--- src/main/java/com/agrocesar/scheduler/AlertaScheduler.java
+++ src/main/java/com/agrocesar/scheduler/AlertaScheduler.java
@@ -5,6 +5,9 @@ import com.agrocesar.model.Usuario;
 import com.agrocesar.service.AlertaService;
 import com.agrocesar.service.SmsService;
 import com.agrocesar.service.UsuarioService;
+
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -29,13 +32,31 @@ public class AlertaScheduler {
     private final SmsService smsService;
     private final UsuarioService usuarioService;
 
-    public AlertaScheduler(AlertaService alertaService,SmsService smsService, UsuarioService usuarioService){
+    public AlertaScheduler(AlertaService alertaService, SmsService smsService, UsuarioService usuarioService) {
         this.alertaService = alertaService;
         this.smsService = smsService;
         this.usuarioService = usuarioService;
     }
 
-    
-    
+    /**
+     * @Scheduled Indica cuantas veces se ejecturara dicho metodo
+     * @cron
+     *       0 = El segundo en el que inicia
+     *       0 = El minuto de la ejecucion
+     *       11 = La hora en formato 24 horas
+     *       * = Cualquier dia del mes
+     *       * = Cualquier mes
+     *       * = Cualquier dia de la semana
+     */
+    @Scheduled(cron = " 0 0 11 * * *", zone = "America/Bogota")
+    public void ejecutarManiana() {
+        log.info("[Scheduler] Ejecucion matutina 6AM");
+    }
+
+    @Scheduled(cron = " 0 0 23 * * *", zone = "America/Bogota")
+    public void ejecutarTarde() {
+        log.info("[Scheduler] Ejecucion vespertina 6PM");
+
+    }
 
 }
